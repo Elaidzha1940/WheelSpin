111e8c3c6bd12e83e727fadecc8d4942aeb91fee
Elaidzha1940 <eli080293@gmail.com>
2023-08-09 08:18:33 +0300

    WheelSpin

---
+++ WheelSpin/WheelSpinApp.swift
@@ -0,0 +1,17 @@
+//
+//  WheelSpinApp.swift
+//  WheelSpin
+//
+//  Created by Elaidzha Shchukin on 09.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct WheelSpinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

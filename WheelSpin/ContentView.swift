b7ee32bcb02d0047ee472c44b65a0e199a3a4bb0
Elaidzha1940 <eli080293@gmail.com>
2023-08-09 10:50:23 +0300

    WheelSpin

--- WheelSpin/ContentView.swift
+++ WheelSpin/ContentView.swift
@@ -20,7 +20,7 @@ struct Pie: Shape {
         let radius = min(rect.width, rect.height) / 2
         let start = CGPoint(
             x: center.x + radius * cos(CGFloat(startAngle.radians)),
-            y: center.y + radius * sin(CGFloat(endAngle.radians))
+            y: center.y + radius * sin(CGFloat(startAngle.radians))
         )
         var path = Path()
         path.move(to: center)
@@ -32,48 +32,51 @@ struct Pie: Shape {
 }
 
 struct ContentView: View {
-    let colors: [Color] = [.blue, .red, .white, .orange, .clear, .cyan, .blue, .black, .blue, .black, .brown, .blue]
+    let colors: [Color] = [.red, .blue, .green, .yellow, .purple, .orange, .brown, .pink, .purple, .primary, .gray, .mint, .red, .blue, .green, .yellow, .pink, .orange]
     
+    @State var spin : Double = 0
     @State var count = 0
-    @State var spin: Double = 0
     
     var body: some View {
         
         VStack {
             ZStack {
                 ForEach(0..<count, id: \.self) { index in
-                    Pie(startAngle: .degrees(Double(index) / Double(count) * 360), endAngle: .degrees(Double(index + 1 ) / Double(count) * 360))
+                    Pie(startAngle: .degrees(Double(index) / Double(count) * 360), endAngle: .degrees(Double(index + 1) / Double(count) * 360))
                         .fill(colors[index % colors.count])
                 }
             }
-            .rotationEffect(.degrees(spin))
+           .rotationEffect(.degrees(spin))
             
-            Spacer()
+           Spacer()
             
-            Button {
-                if count < colors.count {
-                    count += 1
-                } else {
-                    count = 0
+            HStack {
+                Button {
+                    if count < colors.count {
+                        count += 1
+                    } else {
+                        count = 0
+                    }
+                } label: {
+                    Text("Add Color")
                 }
-            } label: {
-                Text("Add Color")
-            }
-            .frame(width: 200, height: 50)
-            .background(.black)
-            .cornerRadius(10)
-            
-            Button {
-                withAnimation(.spring(response: 2, dampingFraction: 1.5)) {
-                    spin += 360
+                .foregroundColor(.red)
+                .frame(width: 200, height: 50)
+                .background(.black)
+                .cornerRadius(10)
+                
+                Button {
+                    withAnimation(.spring(response: 2, dampingFraction: 1.5)) {
+                        spin += 360
+                    }
+                } label: {
+                    Text("Spin")
                 }
-            } label: {
-                <#code#>
+                .foregroundColor(.red)
+                .frame(width: 200, height: 50)
+                .background(.black)
+                .cornerRadius(10)
             }
-            .frame(width: 200, height: 50)
-            .background(.black)
-            .cornerRadius(10)
-
         }
         .padding()
     }

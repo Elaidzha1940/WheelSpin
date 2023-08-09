a6d5f0093384f0507dfc63ee25acb0664d2d4906
Elaidzha1940 <eli080293@gmail.com>
2023-08-09 12:16:03 +0300

    Done

--- WheelSpin/WheelSpinApp.swift
+++ WheelSpin/WheelSpinApp.swift
@@ -1,9 +1,11 @@
+//  /;
 //
-//  WheelSpinApp.swift
-//  WheelSpin
+//  Project: WheelSpin
+//  File: WheelSpinApp.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 09.08.2023
 //
-//  Created by Elaidzha Shchukin on 09.08.2023.
-//
+//  */
 
 import SwiftUI
 

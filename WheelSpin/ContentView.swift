8ce60dbece6644efe15fc3cad2877486e34d8c50
Elaidzha1940 <eli080293@gmail.com>
2023-08-09 08:32:31 +0300

    WheelSpin + added Pie Shape

--- WheelSpin/ContentView.swift
+++ WheelSpin/ContentView.swift
@@ -11,13 +11,34 @@ import SwiftUI
 
 struct ContentView: View {
     
+    struct Pie: Shape {
+        
+        var startAngle: Angle
+        var endAngle: Angle
+        
+        func path(in rect: CGRect) -> Path {
+            
+            let center = CGPoint(x: rect.minX, y: rect.midY)
+            let radius = min(rect.width, rect.height) / 2
+            let start = CGPoint(
+                
+                x: center.x + radius * cos(CGFloat(startAngle.radians)),
+                y: center.y + radius * sin(CGFloat(endAngle.radians))
+            )
+            
+            var path = Path()
+            path.move(to: center)
+            path.addLine(to: start)
+            path.addArc(center: center, radius: radius, startAngle: startAngle, endAngle: endAngle, clockwise: false)
+            path.addLine(to: center)
+            return path
+        }
+    }
     
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+           
         }
         .padding()
     }
